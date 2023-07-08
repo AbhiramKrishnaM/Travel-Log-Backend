d42f129243e04ce5dbcab7f97c5c5e84682ff082
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-07-08 19:52:59 +0530

    added file clean up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const middleware = require("./middleware");
+
 const app = express();
 app.use(morgan("common"));
 app.use(helmet());
@@ -13,20 +15,9 @@ app.use(
 );
 
 // not found middleware
-app.use((req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl} `);
-  res.status(404);
-  next(error);
-});
+app.use(middleware.notFound);
 
-app.use((error, req, res, next) => {
-  const statusCode = error.statuscode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: error.message,
-    stack: process.env.NODE_ENV === "production" ? "" : error.stack,
-  });
-});
+app.use(middleware.errorHandler);
 
 const port = process.env.PORT || 4000;
 

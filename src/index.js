a2b0a72bb499d6b99d0bc82f0fa36e4173afa4a6
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-07-09 00:00:46 +0530

    added log routes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const middleware = require("./middleware");
+// Api
+const logs = require("./api/logs");
 
 const app = express();
 
@@ -19,10 +21,12 @@ app.use(
     origin: process.env.CORS_ORIGIN,
   })
 );
+app.use(express.json());
+
+app.use("/api/logs", logs);
 
 // not found middleware
 app.use(middleware.notFound);
-
 app.use(middleware.errorHandler);
 
 const port = process.env.PORT || 4000;

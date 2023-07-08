58d57f0af5f2de1112ebab33c5c565cfbbf9b2c1
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-07-08 20:47:32 +0530

    added dotenv, added mongoose connect

--- src/models/LogEntry.js
+++ src/models/LogEntry.js
@@ -14,11 +14,15 @@ const logEntrySchema = new Schema(
     comments: String,
     rating: { type: Number, min: 0, max: 10, default: 0 },
     image: String,
-    latitude: requiredNumber,
-    longitude: requiredNumber,
+    latitude: { ...requiredNumber, min: -90, max: 90 },
+    longitude: { ...requiredNumber, min: -180, max: 180 },
     visitDate: { required: true, type: Date },
   },
   {
     timestamps: true,
   }
 );
+
+const LogEntry = mongoose.model("LogEntry", logEntrySchema);
+
+module.exports = LogEntry;
